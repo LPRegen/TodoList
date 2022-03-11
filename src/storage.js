4c8485912f1bbf6c91601a3e9dfb6d7367f0e04e
LPRegen <escri.manu@gmail.com>
2022-03-11 09:48:03 -0300

    Change code structure

--- src/storage.js
+++ src/storage.js
@@ -1,25 +1,128 @@
-'use strict';
+"use strict";
 
-const Storage = (function checkStorage() {
-  let _storedProjects = [];
+import parseISO from "date-fns/parseISO";
+import { formatISO, differenceInDays } from "date-fns";
+
+const DataBase = (function () {
+  let _storage = [];
+  let _projectList = [];
+
+  function _updateDB() {
+    localStorage.setItem("storage", JSON.stringify(_storage));
+  }
 
   (function _checkStorage() {
-    if (localStorage.getItem('projects')) {
-      _storedProjects = JSON.parse(localStorage.getItem('projects'));
+    if (localStorage.getItem("storage")) {
+      _storage = JSON.parse(localStorage.getItem("storage"));
     } else {
-      localStorage.setItem('projects', JSON.stringify(_storedProjects));
+      _updateDB();
     }
   })();
 
-  const addItem = function (item) {
-    _storedProjects.push(item);
-    localStorage.setItem('projects', JSON.stringify(_storedProjects));
-    console.log('pushed');
-  };
+  function readDB() {
+    _storage.forEach((el) => {
+      console.log(el);
+    });
+  }
+
+  function deleteDB() {
+    _storage = [];
+    _updateDB();
+  }
+
+  function addElement(project) {
+    _storage.push(project);
+    _updateDB();
+  }
+
+  function removeProject(project) {
+    let projectName = project.name;
+    let projectIndex;
+    for (let i = 0; i < _storage.length; i++) {
+      if (_storage[i].name === projectName) {
+        projectIndex = i;
+        break;
+      }
+    }
+    _storage.splice(projectIndex, 1);
+    _updateDB();
+  }
 
   return {
-    addItem,
+    readDB,
+    deleteDB,
+    addElement,
+    removeProject,
   };
 })();
 
-export { Storage };
+class Project {
+  name;
+  creationDate;
+  tasksContainer = [];
+
+  constructor(name) {
+    this.name = name;
+    this.creationDate = formatISO(new Date(), { representation: "date" });
+  }
+
+  static deleteTask(project, task) {
+    let taskIndex;
+    for (let i = 0; i < project.tasksContainer.length; i++) {
+      if (project.tasksContainer[i].name === task.name) {
+        taskIndex = i;
+        break;
+      }
+    }
+    project.tasksContainer.splice(taskIndex, 1);
+  }
+}
+
+class Task {
+  creationDate;
+  name;
+  note;
+  parentProject;
+  statusCompleted = false;
+  dueDate;
+
+  constructor(name, note, parentProject, dueDate = undefined) {
+    this.creationDate = formatISO(new Date(), { representation: "date" });
+    this.name = name;
+    this.note = note;
+    this.parentProject = parentProject;
+    this.statusCompleted;
+    this.dueDate = dueDate;
+  }
+
+  static storeNote(task, newNote) {
+    task.note = newNote;
+  }
+  static modifyName(task, newName) {
+    task.name = newName;
+  }
+
+  static checkDate(task) {
+    let status;
+    if (
+      differenceInDays(parseISO(task.dueDate), parseISO(task.creationDate)) <=
+      -1
+    ) {
+      return (status = false);
+    } else {
+      return (status = true);
+    }
+  }
+
+  static linkToProject(task, project) {
+    project.tasksContainer.push(task);
+  }
+
+  static modifyStatus(task) {
+    task.statusCompleted === false
+      ? (task.statusCompleted = true)
+      : (task.statusCompleted = false);
+  }
+}
+
+export { DataBase, Project, Task };
