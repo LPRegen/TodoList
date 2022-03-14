ec486a9c202d6ba5a7aae14852e4c1795366cb15
LPRegen <escri.manu@gmail.com>
2022-03-14 04:12:17 -0300

    Modify code structure

--- src/elements.js
+++ src/elements.js
@@ -1,81 +1,46 @@
 'use strict';
 
-import parseISO from 'date-fns/parseISO';
-import { formatISO, differenceInDays } from 'date-fns';
-import { Storage } from './storage';
+import { differenceInMinutesWithOptions } from 'date-fns/fp';
+import { DataBase, Project, Task } from './storage';
 
-class Project {
-  constructor(name) {
-    this.name = name;
-    this.creationDate = formatISO(new Date(), { representation: 'date' });
-    this.uncompleteTasks = [];
-    this.completeTask = [];
+class ProjectElements {
+  static projectList = document.querySelector('#project-container');
+
+  static createInput() {
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'project-name';
+    ProjectElements.projectList.insertAdjacentElement('afterbegin', input);
+    input.focus();
+    return {
+      input,
+    };
   }
 
-  hasExpired() {
-    if (
-      differenceInDays(parseISO(this.dueDate), parseISO(this.creationDate)) <=
-      -1
-    ) {
-      console.log('has expired');
-    } else {
-      console.log('still available');
-    }
-  }
-}
-
-class Task extends Project {
-  constructor(name, note, parentProject) {
-    super(name);
-    this.note = note;
-    this.parentProject = parentProject;
-  }
-
-  storeNote() {
-    this.parentProject.tasks.push(this);
-  }
-}
-
-const CreateElements = (function () {
-  let _projectCount = 0;
-  const _projectList = document.querySelector('#project-container');
-
-  function _projectHTML(name) {
-    let projectContainer = document.createElement('div');
-    let projectName = document.createElement('p');
-    projectContainer.dataset.projectCount = _projectCount++;
-    projectName.textContent = name;
-    projectContainer.classList.add('projects');
-    projectContainer.append(projectName);
-    _projectList.insertAdjacentElement('beforeend', projectContainer);
-  }
-
-  (function _readStorage() {
-    Storage.showProjects().forEach((project) => {
-      _projectHTML(project.name);
-    });
-  })();
-
-  const createProjectInstance = function () {
-    let projectInput = document.createElement('input');
-    projectInput.type = 'text';
-    projectInput.id = 'project-name';
-    _projectList.insertAdjacentElement('beforeend', projectInput);
-    projectInput.focus();
-    projectInput.onblur = function () {
-      if (projectInput.value !== '' && projectInput.value !== ' ') {
-        const project = new Project(projectInput.value);
-        Storage.addItem(project);
-        _projectHTML(projectInput.value);
-        projectInput.value = '';
+  static createHTMLProject() {
+    let projectInput = ProjectElements.createInput().input;
+    projectInput.onblur = () => {
+      if (
+        projectInput.value !== '' &&
+        projectInput.value !== ' ' &&
+        !DataBase.isDuplicated(projectInput.value)
+      ) {
+        let newProject = new Project(projectInput.value);
+        DataBase.addProject(newProject);
+        const container = document.createElement('div');
+        const title = document.createElement('p');
+        container.classList.add('projects');
+        container.append(title);
+        console.log(newProject);
+        title.textContent = newProject.name;
+        ProjectElements.projectList.insertAdjacentElement(
+          'afterbegin',
+          container
+        );
       }
       projectInput.remove();
     };
-  };
+  }
+}
 
-  return {
-    createProjectInstance,
-  };
-})();
-
-export { CreateElements };
+export { ProjectElements };
